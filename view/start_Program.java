5b20645fa0570990ae2126960537311574788ed8
CodeSucker03 <132595138+CodeSucker03@users.noreply.github.com>
2024-05-16 21:46:59 +0700

    Delete view directory

--- view/start_Program.java
+++
@@ -1,8 +0,0 @@
-package loginPrj;
-
-public class start_Program {
-    public static void main(String[] args) {
-        @SuppressWarnings("unused")
-        Login_View loginFrame = new Login_View();
-    }
-}
